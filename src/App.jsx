0a401a7b29b7a44b82f9a2a34a09f1515fb22313
alejandroMazas <alejandro.mazasdelizana@gmail.com>
2025-05-27 21:30:12 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Player from "./components/Player"
+
+
+function App() {
+
+
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+
+        Game Board
+
+      </div>
+      log
+    </main>
+  )
+}
+
+export default App
